7a50d5ff3303b889774fef031a64717636c09b71
Muhammad Zaman <artfunjoin@gmail.com>
2022-08-26 01:37:55 +0500

    delet product

--- routes/admin.js
+++ routes/admin.js
@@ -38,4 +38,17 @@ adminRouter.get('/admin/get-products',admin,async(req ,res)=>{
     }
 });
 
+// Delete products
+
+adminRouter.post('/admin/delete-products',admin,async(req , res)=>{
+    try{
+        const { id } = req.body;
+        let product = await Product.findByIdAndDelete(id);
+        product = await product.save();
+        res.json(product);
+    }catch(e){
+        res.status(500).json({error:e.message});
+    }
+});
+
 module.exports = adminRouter;
