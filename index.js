cbca4a8f593d81e840c0bfd300951880fff729d2
Muhammad Zaman <artfunjoin@gmail.com>
2022-08-26 15:35:55 +0500

    ok

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const adminRouter = require("./routes/admin");
 
 
 const authRoter =require("./routes/auth");
+const productRouter = require("./routes/product");
 
 const PORT = process.env.PORT || 3000;
 const DB = "mongodb+srv://zaman:1234567890@cluster0.absrvai.mongodb.net/?retryWrites=true&w=majority";
@@ -12,6 +13,7 @@ const app =express();
 app.use(express.json());
 app.use(authRoter);
 app.use(adminRouter);
+app.use(productRouter);
 
 
 // Connections
