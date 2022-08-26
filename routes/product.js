14a742fe97e6726f88917065c497cafe658b44fe
Muhammad Zaman <artfunjoin@gmail.com>
2022-08-26 15:32:41 +0500

    category

---
+++ routes/product.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const productRouter = express.Router();
+const auth = require('../middlewares/auth');
+const { Product } = require('../models/product');
+
+// Get product by category
+
+// if we want /api/products?category=Mobile => req.query.category
+// if we want /api/products:category=Mobile => req.params.category
+
+productRouter.get('/api/products',auth,async(req ,res)=>{
+    try{
+        const product = await Product.find({category : req.query.category});
+        res.json(product);
+    }catch(e){
+        res.status(500).json({error:e.message});
+    }
+});
+
+
+module.exports = productRouter;
